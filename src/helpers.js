fd781f5b52be22494aa2c150e0fbe142b60c3b01
wojtrawi <wojtrawi@gmail.com>
2017-12-01 12:44:16 +0100

    Enables adding new contact to redux store

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,9 @@
+import store from "./store";
+
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default (async function showResults(values) {
     await sleep(500);
     window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
+    store.dispatch({type: 'ADD_CONTACT', newContact: values})
 });
--- src/store.js
+++ src/store.js
@@ -1,8 +1,18 @@
 import { createStore, combineReducers } from 'redux';
 import { reducer as reduxFormReducer } from 'redux-form';
 
+const contactsReducer = (state = [], action = {}) => {
+    switch (action.type) {
+        case 'ADD_CONTACT':
+            return [...state, action.newContact]
+        default:
+            return state
+    }
+}
+
 const reducer = combineReducers({
     form: reduxFormReducer,
+    contacts: contactsReducer
 });
 const store = (window.devToolsExtension
     ? window.devToolsExtension()(createStore)
