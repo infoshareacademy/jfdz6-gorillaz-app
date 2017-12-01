c901b7f8481abeb4605f126097aac8c950bb34ef
wojtrawi <wojtrawi@gmail.com>
2017-12-01 23:06:45 +0100

    Add Bootstrap styles to AddContactForm

--- src/AddContactForm.js
+++ src/AddContactForm.js
@@ -1,68 +1,89 @@
 import React from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {
+    FormGroup,
+    ControlLabel,
+    FormControl,
+    HelpBlock,
+    Button,
+    ButtonToolbar
+} from 'react-bootstrap'
 
 import validate from './addContactFormValidation'
 
-const renderField = ({input, label, type, meta: {touched, error}}) => (
-    <div>
-        <label>{label}</label>
-        <div>
-            <input {...input} placeholder={label} type={type}/>
-            {touched && (error && <span>{error}</span>)}
-        </div>
-    </div>
+const renderTextField = ({input, label, type, meta: {touched, error}}) => (
+    <FormGroup
+        validationState={touched ? (error ? 'error' : 'success') : null}
+    >
+        <ControlLabel>{label}</ControlLabel>
+        <FormControl
+            {...input}
+            type={type}
+            placeholder={label}
+        />
+        <FormControl.Feedback/>
+        <HelpBlock>{touched && error ? error : null}</HelpBlock>
+    </FormGroup>
 )
+
+const renderTextareaField = ({input, label, type}) => (
+    <FormGroup>
+        <ControlLabel>{label}</ControlLabel>
+        <FormControl {...input} componentClass={type} placeholder={label} />
+    </FormGroup>
+)
+
 
 const AddContactForm = props => {
     const {handleSubmit, pristine, reset, submitting} = props;
     return (
-        <form onSubmit={handleSubmit}>
-            <Field
-                name="firstName"
-                component={renderField}
-                type="text"
-                label="First Name"
-            />
-            <Field
-                name="lastName"
-                component={renderField}
-                type="text"
-                label="Last Name"
-            />
-            <Field
-                name="email"
-                component={renderField}
-                type="email"
-                label="Email"
-            />
-            <div>
-                <label>Sex</label>
-                <div>
-                    <label>
-                        <Field name="sex" component="input" type="radio" value="male"/>
-                        {' '}
-                        Male
-                    </label>
-                    <label>
-                        <Field name="sex" component="input" type="radio" value="female"/>
-                        {' '}
-                        Female
-                    </label>
-                </div>
-            </div>
-            <div>
-                <label>Notes</label>
-                <div>
-                    <Field name="notes" component="textarea"/>
-                </div>
-            </div>
-            <div>
-                <button type="submit" disabled={pristine || submitting}>Submit</button>
-                <button type="button" disabled={pristine || submitting} onClick={reset}>
-                    Clear Values
-                </button>
-            </div>
-        </form>
+        <div className="form__wrapper">
+            <form onSubmit={handleSubmit}>
+                <Field
+                    name="firstName"
+                    component={renderTextField}
+                    type="text"
+                    label="First Name"
+                />
+                <Field
+                    name="lastName"
+                    component={renderTextField}
+                    type="text"
+                    label="Last Name"
+                />
+                <Field
+                    name="email"
+                    component={renderTextField}
+                    type="email"
+                    label="Email"
+                />
+                <FormGroup>
+                    <label>Sex</label>
+                    <div>
+                        <label>
+                            <Field name="sex" component="input" type="radio" value="male"/>
+                            {' '}
+                            Male
+                        </label>
+                        <label>
+                            <Field name="sex" component="input" type="radio" value="female"/>
+                            {' '}
+                            Female
+                        </label>
+                    </div>
+                </FormGroup>
+                <Field
+                    name="notes"
+                    component={renderTextareaField}
+                    type="textarea"
+                    label="Notes"
+                />
+                <ButtonToolbar>
+                    <Button type="submit" bsStyle="success" disabled={pristine || submitting}>{'Submit' + (submitting ? 'ting' : '')}</Button>
+                    <Button type="button" bsStyle="warning" disabled={pristine || submitting} onClick={reset}>Clear Values</Button>
+                </ButtonToolbar>
+            </form>
+        </div>
     );
 };
 
