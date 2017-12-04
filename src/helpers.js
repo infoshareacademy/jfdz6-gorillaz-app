d1fc7ee1047b98709a1c3d2a608b0934b103ea3e
wojtrawi <wojtrawi@gmail.com>
2017-12-04 18:28:03 +0100

    Add unique 'id' property for each added contact

--- src/helpers.js
+++ src/helpers.js
@@ -6,5 +6,5 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 export default (async function showResults(values) {
     await sleep(500)
     window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
-    store.dispatch(add(values))
+    store.dispatch(add({id: Date.now(), ...values}))
 });
