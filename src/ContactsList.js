2f4d2ef656d5732f49cfce09b03580cf9ceadeab
wojtrawi <wojtrawi@gmail.com>
2017-12-04 19:48:37 +0100

    Enable contact deletion

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -1,17 +1,32 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {Button} from 'react-bootstrap'
 
-export default class ContactsList extends React.Component {
+import {remove} from './state/contacts'
+
+class ContactsList extends React.Component {
+    handleRemoveContactClick = (event) => {
+        const contactId = +event.target.dataset.contactId
+
+        this.props.removeContact(contactId)
+    }
+
     render() {
         const contactsList = this.props.contacts.map(
             contact => (
                 <li key={contact.id}>
                     {`${contact.firstName} ${contact.lastName}`}
                     <Button
-                    onClick={this.props.onEditContact}
-                    data-contact-id={contact.id}
+                        onClick={this.props.onEditContact}
+                        data-contact-id={contact.id}
                     >
                         Edit
+                    </Button>
+                    <Button
+                        onClick={this.handleRemoveContactClick}
+                        data-contact-id={contact.id}
+                    >
+                        Delete
                     </Button>
                 </li>
             )
@@ -23,3 +38,12 @@ export default class ContactsList extends React.Component {
         )
     }
 }
+
+const mapDispatchToProps = dispatch => ({
+    removeContact: (contactId) => dispatch(remove(contactId))
+})
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(ContactsList)
--- src/state/contacts.js
+++ src/state/contacts.js
@@ -1,8 +1,14 @@
 const ADD = 'contacts/ADD_CONTACT'
+const REMOVE = 'contacts/REMOVE_CONTACT'
 
 export const add = newContact => ({
     type: ADD,
     newContact
+})
+
+export const remove = contactId => ({
+    type: REMOVE,
+    contactId
 })
 
 const initialState = []
@@ -11,6 +17,11 @@ export default (state = initialState, action = {}) => {
     switch (action.type) {
         case ADD:
             return [...state, action.newContact]
+        case REMOVE:
+            return (state.filter(
+                    contact => contact.id !== action.contactId
+                )
+            )
         default:
             return state
     }
