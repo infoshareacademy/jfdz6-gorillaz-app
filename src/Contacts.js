8d1d77cca4cab136336a6fde4ddecd223fa5e72b
wojtrawi <wojtrawi@gmail.com>
2017-12-04 18:20:31 +0100

    Add button navigating to new contact's form

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Button} from 'react-bootstrap'
 
 import AddContactForm from './AddContactForm'
 import showResults from "./helpers";
 import ContactsList from './ContactsList'
 
 class Contacts extends React.Component {
+    state = {
+        addingContact: false,
+        selectedContact: null
+    }
+
+    handleAddContactClick = () => {
+        this.setState({addingContact: true})
+    }
 
     render() {
         return (
             <div>
                 <h1>List of contacts</h1>
-                <AddContactForm onSubmit={showResults}/>
+                <Button onClick={this.handleAddContactClick}>Add Contact</Button>
+                {this.state.addingContact ? <AddContactForm onSubmit={showResults}/>
+                    : <h4>Click button to add new contact or select item from the list</h4>}
                 <ContactsList contacts={this.props.contacts}/>
             </div>
         )
