9dd37b8d21d5c0a150568657de1b7b22635b19f5
wojtrawi <wojtrawi@gmail.com>
2017-12-08 09:48:35 +0100

    Add public holidays data

---
+++ src/events/public-movable-holidays.js
@@ -0,0 +1,18 @@
+export const pubilcMovableHolidays = [
+    {
+        title: 'Easter Sunday',
+        payload: 'Placeholder'
+    },
+    {
+        title: 'Easter Monday',
+        payload: 'Placeholder'
+    },
+    {
+        title: 'Pentecost Sunday',
+        payload: 'Placeholder'
+    },
+    {
+        title: 'Corpus Christi',
+        payload: 'Placeholder'
+    },
+]
