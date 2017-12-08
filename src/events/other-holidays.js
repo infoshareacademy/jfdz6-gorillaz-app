3b2e99dcc62b7b4eca81404cdbe4f0a295a5b028
wojtrawi <wojtrawi@gmail.com>
2017-12-08 09:59:13 +0100

    Add other common holidays data

---
+++ src/events/other-holidays.js
@@ -0,0 +1,67 @@
+export const otherHolidays =[
+    {
+        date: '2101',
+        title: 'Grandmother\'s Day',
+        payload: 'Placeholder'
+    },
+    {
+        date: '2201',
+        title: 'Grandfather\'s Day',
+        payload: 'Placeholder'
+    },
+    {
+        date: '1402',
+        title: 'Valentine\'s Day',
+        payload: 'Placeholder'
+    },
+    {
+        date: '0803',
+        title: 'Women\'s Day',
+        payload: 'Placeholder'
+    },
+    {
+        date: '2605',
+        title: 'Mother\'s Day',
+        payload: 'Placeholder'
+    },
+    {
+        date: '0106',
+        title: 'Children\'s Day',
+        payload: 'Placeholder'
+    },
+    {
+        date: '2306',
+        title: 'Father\'s Day',
+        payload: 'Placeholder'
+    },
+    {
+        date: '3009',
+        title: 'Boy\'s Day',
+        payload: 'Placeholder'
+    },
+    {
+        date: '1410',
+        title: 'Teachers\' Day',
+        payload: 'Placeholder'
+    },
+    {
+        date: '3110',
+        title: 'Halloween',
+        payload: 'Placeholder'
+    },
+    {
+        date: '0612',
+        title: 'Santa Claus Day',
+        payload: 'Placeholder'
+    },
+    {
+        date: '2412',
+        title: 'Christmas Eve',
+        payload: 'Placeholder'
+    },
+    {
+        date: '3112',
+        title: 'New Year\'s Eve',
+        payload: 'Placeholder'
+    },
+]
