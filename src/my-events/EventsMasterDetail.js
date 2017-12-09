0988b3ce22de2b1e688696cf6123805d5c06b8db
wojtrawi <wojtrawi@gmail.com>
2017-12-09 13:54:46 +0100

    Enable adding new event in 'My events' section

--- src/my-events/EventsMasterDetail.js
+++ src/my-events/EventsMasterDetail.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import {add} from "../state/custom-events"
 import EventsMasterView from './EventsMasterView'
+import EventForm from '../events/EventForm'
 
 class EventsMasterDetail extends React.Component {
     state = {
@@ -18,12 +20,34 @@ class EventsMasterDetail extends React.Component {
         })
     }
 
+    handleSubmit = newEvent => {
+        this.props.addEvent(newEvent)
+    }
+
     render() {
+        const date = new Date()
+        const parsedDate =
+            [
+                date.getFullYear(),
+                ('0' + (date.getMonth() + 1)).slice(-2),
+                ('0' + date.getDate()).slice(-2)
+            ]
+                .join('-')
+
         return (
-            <EventsMasterView
-                events={this.props.events}
-                onEventClick={this.handleEventClick}
-            />
+            <div>
+                <h2>Your custom events</h2>
+                <EventsMasterView
+                    events={this.props.events}
+                    onEventClick={this.handleEventClick}
+                />
+                <h2>Add new event</h2>
+                <EventForm
+                    onSubmit={this.handleSubmit}
+                    initialValues={{date: parsedDate}}
+                />
+            </div>
+
         )
     }
 }
@@ -32,6 +56,11 @@ const mapStateToProps = state => ({
     events: state.customEvents
 })
 
+const mapDispatchToProps = dispatch => ({
+    addEvent: (newEvent) => dispatch(add(newEvent))
+})
+
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(EventsMasterDetail)
