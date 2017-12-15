3685601bbaa81066fb2a1d99959ead651865b668
wojtrawi <wojtrawi@gmail.com>
2017-12-15 23:23:51 +0100

    Add edit contact view

---
+++ src/contacts/views/EditContact.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+import {add, remove} from "../../state/contacts"
+import getContactForm from '../form/contact-form-factory'
+
+class EditContact extends React.Component {
+    handleSubmit = newContact => {
+        this.props.addContact(newContact)
+        this.props.removeContact(this.props.item.id)
+    }
+
+    render() {
+        const {item} = this.props
+        const EditContactForm = getContactForm('editContactForm' + item.id)
+
+        return (
+            <EditContactForm
+                onSubmit={this.handleSubmit}
+                initialValues={{...item}}
+                cancelButton ={this.props.children}
+            />
+        )
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    addContact: (newContact) => dispatch(add(newContact)),
+    removeContact: (contactId) => dispatch(remove(contactId))
+})
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(EditContact)
