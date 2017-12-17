d8f2b55aebb1a68e6c013c9405bbf8e90c4f652d
Piotr Kramarz <piotr.m.kramarz@gmail.com>
2017-12-17 11:40:07 +0100

    Introduce firebase, create authentication branch

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import SignUp from './SignUp'
+
 import {
     BrowserRouter as Router,
     Route
@@ -50,6 +52,7 @@ const MainRouter = () => (
             </Navbar>
 
             <Route exact path="/" component={Home}/>
+            <Route path="/sign-up" component={SignUp}/>
             <Route path="/about" component={About}/>
             <Route path="/contacts" component={ContactsMasterDetail}/>
             <Route path="/calendar" component={Calendar}/>
--- src/store.js
+++ src/store.js
@@ -2,10 +2,21 @@ import { compose, createStore, combineReducers, applyMiddleware } from 'redux'
 import persistState from 'redux-localstorage'
 import thunk from 'redux-thunk'
 import { reducer as reduxFormReducer } from 'redux-form';
+import firebase from 'firebase'
 
 import contacts from './state/contacts'
 import customEvents from './state/custom-events'
 import holidays from './state/holidays'
+
+const config = {
+  apiKey: "AIzaSyC4j75g_NETl374jKo_KDbETITmzcGxYMs",
+  authDomain: "gorillaz-b1166.firebaseapp.com",
+  databaseURL: "https://gorillaz-b1166.firebaseio.com",
+  projectId: "gorillaz-b1166",
+  storageBucket: "gorillaz-b1166.appspot.com",
+  messagingSenderId: "699741243303"
+};
+firebase.initializeApp(config);
 
 const reducer = combineReducers({
     form: reduxFormReducer,
