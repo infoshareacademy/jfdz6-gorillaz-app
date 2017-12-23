b7c6d5c47e80edbfc67e0b1a6cb8b5257675936a
wojtrawi <wojtrawi@gmail.com>
2017-12-23 23:22:26 +0100

    Store contacts data in firebase database

--- src/contacts/ContactsMasterDetail.js
+++ src/contacts/ContactsMasterDetail.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
+import {
+    subscribeContacts,
+    unsubscribeContacts
+} from '../state/contacts'
 import MasterDetail from '../generic-master-detail/MasterDetail'
 import NewContact from './views/NewContact'
 import PreviewContact from './views/PreviewContact'
 import DetailedContact from './views/DetailedContact'
 import EditContact from './views/EditContact'
 
-const ContactsMasterDetail = props => {
+class ContactsMasterDetail extends React.Component {
+    componentDidMount = () => {
+        this.props.subscribeContacts()
+    }
 
-    return (
+    componentWillUnmount = () => {
+        this.props.unsubscribeContacts()
+    }
+
+    render = () => (
         <MasterDetail
-            items={props.items}
+            items={this.props.items}
             name={'contact'}
             NewItem={NewContact}
             PreviewItem={PreviewContact}
@@ -25,6 +36,12 @@ const mapStateToProps = state => ({
     items: state.contacts
 })
 
+const mapDispatchToProps = dispatch => ({
+    subscribeContacts: () => dispatch(subscribeContacts()),
+    unsubscribeContacts: () => dispatch(unsubscribeContacts())
+})
+
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(ContactsMasterDetail)
