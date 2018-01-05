525e3d0b9b89672ecd22e2290f0942d36aba0aac
wojtrawi <wojtrawi@gmail.com>
2018-01-06 00:43:41 +0100

    Clean up event form factory code

--- src/shared-utils/events/event-form-factory.js
+++ src/shared-utils/events/event-form-factory.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import {reset, reduxForm} from 'redux-form'
 
-import EventForm from '../../shared-components/EventForm/EventForm'
 import validate from './event-form-validation'
+import EventForm from '../../shared-components/EventForm/EventForm'
 
 const getEventForm = (formName, resetDelay) => {
-    const onSubmitSuccess = (result, dispatch) => {
-        resetDelay && setTimeout(() => dispatch(reset(formName)), resetDelay)
-    }
+    const onSubmitSuccess = (result, dispatch) => resetDelay && setTimeout(() => dispatch(reset(formName)), resetDelay)
 
     return reduxForm({
         form: formName,
