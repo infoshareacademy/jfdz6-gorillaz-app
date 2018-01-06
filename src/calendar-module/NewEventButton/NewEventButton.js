946d2269301de2bfaa27b40f475b8c1b7af3364f
wojtrawi <wojtrawi@gmail.com>
2018-01-06 22:42:36 +0100

    Refactor NewEventButton component code
    - use NewEvent component to render itself

--- src/calendar-module/NewEventButton/NewEventButton.js
+++ src/calendar-module/NewEventButton/NewEventButton.js
@@ -1,37 +1,17 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import moment from 'moment'
 
-import {addEvent} from "../../state/custom-events"
-import getEventForm from '../../shared-utils/events/event-form-factory'
 import ModalButton from '../../shared-components/ModalButton/ModalButton'
+import NewEvent from '../../events-crud-module/NewEvent/NewEvent'
 
-class NewEventButton extends React.Component {
-    handleSubmit = newEvent => this.props.addEvent(newEvent)
-
-    render() {
-        const currentDate = moment().format("YYYY-MM-DD")
-        const NewEventForm = getEventForm('newEventForm', 2000)
-
-        return (
-            <ModalButton
-                buttonType="AddEventButton"
-                buttonName="+"
-                modalGlyph="plus"
-                modalTitle="Add new event"
-            >
-                <NewEventForm
-                    onSubmit={this.handleSubmit}
-                    initialValues={{date: currentDate}}
-                />
-            </ModalButton>
-        )
-    }
+export default class NewEventButton extends React.Component {
+    render = () => (
+        <ModalButton
+            buttonType="AddEventButton"
+            buttonName="+"
+            modalGlyph="plus"
+            modalTitle="Add new event"
+        >
+            <NewEvent/>
+        </ModalButton>
+    )
 }
-
-const mapDispatchToProps = dispatch => ({addEvent: newEvent => dispatch(addEvent(newEvent))})
-
-export default connect(
-    null,
-    mapDispatchToProps
-)(NewEventButton)
