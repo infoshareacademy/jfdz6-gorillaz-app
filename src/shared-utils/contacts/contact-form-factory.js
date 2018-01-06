f08f06655be595700e17729552449a40ac9d9037
wojtrawi <wojtrawi@gmail.com>
2018-01-06 21:52:33 +0100

    Clean up contact-form-factory code

--- src/shared-utils/contacts/contact-form-factory.js
+++ src/shared-utils/contacts/contact-form-factory.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import {reset, reduxForm} from 'redux-form'
 
-import ContactForm from '../../shared-components/ContactForm/ContactForm'
 import validate from './contact-form-validation'
+import ContactForm from '../../shared-components/ContactForm/ContactForm'
 
 const getContactForm = (formName, resetDelay) => {
-    const onSubmitSuccess = (result, dispatch) => {
-        resetDelay && setTimeout(() => dispatch(reset(formName)), resetDelay)
-    }
+    const onSubmitSuccess = (result, dispatch) => resetDelay && setTimeout(() => dispatch(reset(formName)), resetDelay)
 
     return reduxForm({
         form: formName,
