c3d45a850ad9f7e7c3f27197ded8cbdc7b874ae2
wojtrawi <wojtrawi@gmail.com>
2018-01-06 19:33:08 +0100

    Clean up DetailView component code

--- src/shared-components/DetailView/DetailView.js
+++ src/shared-components/DetailView/DetailView.js
@@ -4,58 +4,39 @@ import {Glyphicon} from 'react-bootstrap'
 import {RectButton} from '../../styled-components/button-components'
 
 export default class EventDetailView extends React.Component {
-    state = {
-        isBeingEdited: false
-    }
+    state = {isBeingEdited: false}
 
-    componentWillReceiveProps = () => {
-        this.setState({
-            isBeingEdited: false
-        })
-    }
+    componentWillReceiveProps = () => this.setState({isBeingEdited: false})
 
-    handleEditItemClick = () => (
-        this.setState({
-            isBeingEdited: true
-        })
-    )
+    handleEditItemClick = () => this.setState({isBeingEdited: true})
 
-    handleCancelClick = () => (
-        this.setState({
-            isBeingEdited: false
-        })
-    )
+    handleCancelClick = () => this.setState({isBeingEdited: false})
 
     render() {
         const {item, DetailedItem, EditItem} = this.props
 
         return (
             this.state.isBeingEdited ?
-                <div>
-                    <EditItem item={item}>
-                        <RectButton
-                            bgc={'#2196f3'}
-                            onClick={this.handleCancelClick}
-                        >
-                            <Glyphicon glyph="remove-circle"/>
-                            {' '}
-                            Cancel
-                        </RectButton>
-                    </EditItem>
-                </div> :
-
-                <div>
-                    <DetailedItem item={item}>
-                        <RectButton
-                            bgc={'#4caf50'}
-                            onClick={this.handleEditItemClick}
-                        >
-                            <Glyphicon glyph="pencil"/>
-                            {' '}
-                            Edit
-                        </RectButton>
-                    </DetailedItem>
-                </div>
+                <EditItem item={item}>
+                    <RectButton
+                        bgc={'#2196f3'}
+                        onClick={this.handleCancelClick}
+                    >
+                        <Glyphicon glyph="remove-circle"/>
+                        {' '}
+                        Cancel
+                    </RectButton>
+                </EditItem> :
+                <DetailedItem item={item}>
+                    <RectButton
+                        bgc={'#4caf50'}
+                        onClick={this.handleEditItemClick}
+                    >
+                        <Glyphicon glyph="pencil"/>
+                        {' '}
+                        Edit
+                    </RectButton>
+                </DetailedItem>
         )
     }
 }
