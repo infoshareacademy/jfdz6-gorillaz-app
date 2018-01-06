44d231667c9de096259b9cf4cdb5fb69a885f17f
wojtrawi <wojtrawi@gmail.com>
2018-01-07 00:21:28 +0100

    Replace css styling with styled-components in PreviewContact component

--- src/contacts-crud-module/PreviewContact/PreviewContact.js
+++ src/contacts-crud-module/PreviewContact/PreviewContact.js
@@ -1,19 +1,17 @@
 import React from 'react'
 
-import './PreviewContact.css'
+import {PreviewHeader, PreviewParagraph} from '../../styled-components/master-detail-components'
 
 const PreviewContact = props => {
     const {item} = props
 
     return (
-        <div className="PreviewContact__wrapper">
-            <h4 className="PreviewContact__header">
+        <div>
+            <PreviewHeader>
                 {`${item.firstName.charAt(0)}. ${item.lastName}`}
-            </h4>
+            </PreviewHeader>
 
-            <p className="PreviewContact__paragraph">
-                {item.email}
-            </p>
+            <PreviewParagraph>{item.email}</PreviewParagraph>
         </div>
     )
 }
