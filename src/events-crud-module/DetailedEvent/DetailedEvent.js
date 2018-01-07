317e4412502b69c817ea0ea82ba5be4e614b54b4
wojtrawi <wojtrawi@gmail.com>
2018-01-07 13:01:41 +0100

    Refactor DetailedEvent component code
    - use form's static controls factory to render event's details instead
    of repeated code

--- src/events-crud-module/DetailedEvent/DetailedEvent.js
+++ src/events-crud-module/DetailedEvent/DetailedEvent.js
@@ -1,50 +1,39 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import moment from 'moment'
-import {FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
 
+import {renderTextField, renderTextareaField} from '../../shared-utils/form-static-controls-factory'
 import {removeEvent} from "../../state/custom-events"
 import DetailedItemToolbar from '../../shared-components/DetailedItemToolbar/DetailedItemToolbar'
 
 import {Wrapper} from '../../styled-components/miscellaneous-components'
-import {DetailedParagraph, DetailedDescription} from '../../styled-components/master-detail-components'
 
 class DetailedEvent extends React.Component {
     handleDeleteEventClick = () => this.props.removeEvent(this.props.item.id)
 
+    getEventDetail = ({label, content}) => (label !== 'Description' ? renderTextField : renderTextareaField)
+        .call(null, label, content)
+
     render() {
-        const {item} = this.props
-        const eventsDate = moment(item.date)
+        const {item: event} = this.props
+        const eventDetails = [
+            {
+                label: 'Date',
+                content: moment(event.date).format("Do MMMM [(tracked since] YYYY)")
+            },
+            {
+                label: 'Title',
+                content: event.title
+            },
+            {
+                label: 'Description',
+                content: event.payload
+            }
+        ].map(detailData => this.getEventDetail(detailData))
 
         return (
             <Wrapper>
-                <FormGroup>
-                    <ControlLabel bsClass="control-label-custom">Date</ControlLabel>
-
-                    <FormControl.Static>
-                        <DetailedParagraph>
-                            {eventsDate.format("Do MMMM [(tracked since] YYYY)")}
-                        </DetailedParagraph>
-                    </FormControl.Static>
-                </FormGroup>
-
-                <FormGroup>
-                    <ControlLabel bsClass="control-label-custom">Title</ControlLabel>
-
-                    <FormControl.Static>
-                        <DetailedParagraph>{item.title}</DetailedParagraph>
-                    </FormControl.Static>
-                </FormGroup>
-
-                <FormGroup>
-                    <ControlLabel bsClass="control-label-custom">Description</ControlLabel>
-
-                    <FormControl.Static>
-                        <DetailedDescription>
-                        {item.payload}
-                        </DetailedDescription>
-                    </FormControl.Static>
-                </FormGroup>
+                {eventDetails}
 
                 <DetailedItemToolbar
                     editItemButton={this.props.children}
