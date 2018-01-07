4ee30328fc00811427a77906a67e6391c13514bc
wojtrawi <wojtrawi@gmail.com>
2018-01-08 00:29:14 +0100

    Reformat PreviewContact component's code
    - alias 'item' to 'contact'

--- src/contacts-crud-module/PreviewContact/PreviewContact.js
+++ src/contacts-crud-module/PreviewContact/PreviewContact.js
@@ -3,15 +3,15 @@ import React from 'react'
 import {PreviewHeader, PreviewParagraph} from '../../styled-components/master-detail-components'
 
 const PreviewContact = props => {
-    const {item} = props
+    const {item: contact} = props
 
     return (
         <div>
             <PreviewHeader>
-                {`${item.firstName.charAt(0)}. ${item.lastName}`}
+                {`${contact.firstName.charAt(0)}. ${contact.lastName}`}
             </PreviewHeader>
 
-            <PreviewParagraph>{item.email}</PreviewParagraph>
+            <PreviewParagraph>{contact.email}</PreviewParagraph>
         </div>
     )
 }
